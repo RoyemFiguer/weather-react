b76c8f040cb49992fd84922f2c9b634205dbc77f
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-11 22:27:06 -0600

    Adding basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import styles from  "./App.module.css"
+import Form from "./components/Form/Form"
 
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador de clima</h1>
 
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <Form/>
+      </div>
     </>
   )
 }
