7281008aabc1f66ba1141702ef6edb64d2c9219c
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-15 11:14:19 -0600

    Connecting Axios for API calls

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,22 @@
+import axios from "axios"
+import { SearchType } from "../types"
+
 export default function useWheather() {
-    const fetchWeather = () => {
-        console.log('Consultando...')
+
+    const fetchWeather = async (search : SearchType) => {
+        const appId = import.meta.env.VITE_API_KEY
+        
+        try{
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+            const {data} = await axios.get(geoUrl)
+            console.log(data)
+            
+            console.log(geoUrl)
+
+        }catch(error){
+            console.log(error)
+        }
     }
 
     return {
